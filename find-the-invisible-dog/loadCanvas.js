e115270448bb7d9c28915d3e993d11a8b1741fb3
mingwai chan <mingwai148@gmail.com>
2021-03-10 15:31:05 +0000

    Update all the elements to recreate CSS

--- find-the-invisible-dog/loadCanvas.js
+++ find-the-invisible-dog/loadCanvas.js
@@ -3,30 +3,33 @@ const video = document.querySelector("#video");
 video.width = window.innerWidth;
 video.height = window.innerHeight;
 var canvas = document.querySelector("#canvas");
+canvas.setAttribute('width', '640');
+canvas.setAttribute('height', '480');
 var c = canvas.getContext("2d");
-
 
 //canvas.width = window.innerWidth;
 //canvas.height = window.innerHeight;
-canvas.width = document.body.scrollWidth; //here
-canvas.height = document.body.scrollHeight;
+//canvas.width = document.body.scrollWidth;
+//canvas.height = document.body.scrollHeight;
 
-var canvas1 = document.querySelector("#canvas1");
-var ctx1 = canvas1.getContext("2d");
+var vcanvas = document.querySelector("#vcanvas");
+vcanvas.setAttribute('width', '640');
+vcanvas.setAttribute('height', '480');
+var vctx = vcanvas.getContext("2d");
 
 let model;
 let handX, handY;
 
 //var w = window.innerWidth;
 //var h = window.innerHeight;
-var w = document.body.scrollWidth; // here
-var h = document.body.scrollHeight;
+var w = canvas.width; // here
+var h = canvas.height;
 
 var isStart = false;
 var isSplash = false;
 var isPlay = false;
 
-c.fillStyle = "black";
+c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 document.getElementById("splash-text").innerHTML = "Dogs Come...";
 
